1f78ed8aa1e319e2612aab05eabddf509e624f1f
Khalikova Galiya <xalgalrin@yandex.ru>
2020-07-15 20:12:10 +0300

    add project

---
+++ Globals/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace OnTestAutomation.Globals
+{
+    public class Constants
+    {
+        public const string URL_HOME_PAGE = @"https://www.utest.com/signup/personal";
+        public const int DEFAULT_TIMEOUT = 20;
+    }
+}
